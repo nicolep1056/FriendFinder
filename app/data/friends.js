1ccc4bd5b4a866d9516d9f0ded24cf6e8ed4b5c3
nicolep1056 <nicolep1056@gmail.com>
2018-11-02 18:13:07 -0500

    routing working...not much else

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsData = [
+    {
+        "name": "Nicole",
+        "photo": "https://avatars2.githubusercontent.com/u/41495085?s=400&u=cfd29ef6296e5f270ef2fa014ad74c37a538c12b&v=4",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Jamison",
+        "photo": "",
+        "scores": [
+            4,
+            3,
+            4,
+            3,
+            5,
+            2,
+            4,
+            5,
+            2,
+            1
+        ]
+    }
+];
+
+module.exports = friendsData;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
-var port = process.env.port || 8080;
+var port = process.env.port || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+/* app.use(bodyParser.text());
+ *//* app.use(bodyParser.json({ type: "application/vnd.api+json" })); */
 
-app.use('/static', express.static(path.join(__dirname, 'app/public')));
+app.use(express.static(path.join(__dirname, "/app/public")));
 
-/* require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app); */
+/* require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app); */
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(port, function() {
-    console.log("App listening on port: " + port);
+    console.log("listening on port http://localhost:8000: " + port);
   });
